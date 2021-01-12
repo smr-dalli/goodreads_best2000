a5f5aa59d4d7f2a6cca7f82a16c905c1c6736cbd
Roy Fishermann <mszlesz@gmail.com>
2021-01-12 11:30:11 +0000

    preprocessed dataframe

--- book.py
+++ book.py
@@ -90,19 +90,17 @@ def data_clean(df):
     ## sparse minirating column to get avg_rating and num_ratings
     df['avg_rating'] = df['minirating'].apply(lambda x: (x.split('— ')[0]))
     df['avg_rating'] = df['avg_rating'].apply(lambda x: x.replace(' avg rating', ''))
-    df['avg_rating'] = df['avg_rating'].apply(lambda x: x.replace(' ', ''))
+    df['avg_rating'] = df['avg_rating'].apply(lambda x: x.replace(' ', '').replace('reallylikedit', '').strip())
 
     df['num_ratings'] = df['minirating'].apply(lambda x: (x.split('— ')[1]))
-    df['num_ratings'] = df['num_ratings'].apply(lambda x: x.replace(' ratings', ''))
+    df['num_ratings'] = df['num_ratings'].apply(lambda x: x.replace(' ratings', '').replace(',',''))
 
     return df
 
 def preprocessing(df):
+    df = data_clean(df)
     df.avg_rating = pd.to_numeric(df.avg_rating, errors="coerce")
     df.num_ratings = pd.to_numeric(df.num_ratings, errors="coerce")
-    # df = df.fillna(0)
-    # df['num_ratings'] = df['num_ratings'].astype(int)
-    # df['avg_rating'] = df['avg_rating'].astype(int)
 
     scaler = MinMaxScaler((1, 10))
     df['minmax_norm_rating'] = scaler.fit_transform(df[['avg_rating']])
@@ -120,5 +118,5 @@ if __name__ == '__main__':
     urls = octoparse['Title_URL']
     scrapper(urls)
     ''' 
-    df = pd.read_csv('Best_2000s.csv)
-
+    df = pd.read_csv('Best_2000s.csv')
+    books = preprocessing(df)
